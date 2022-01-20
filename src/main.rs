99f7a72c63182a0caa89c8a5a290141eeabe1143
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2022-01-20 22:22:17 +0200

    Add basic rocket helloworld

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{get, launch, routes};
+
+#[get("/")]
+fn index() -> &'static str {
+  "Hello world"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
