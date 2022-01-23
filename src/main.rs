d8bbbd7e081dafe0b5d4883aedbbce8e3a6bae17
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2022-01-23 12:27:12 +0200

    Wire up routes with okapi and add swagger
    Doesn't compile yet, that's ok

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,56 @@
 #[macro_use]
 extern crate diesel;
 
+use chrono::{DateTime, Utc};
+use diesel::prelude::*;
+use diesel::Queryable;
 use rocket::figment::{
   util::map,
   value::{Map, Value},
 };
-use rocket::{get, launch, routes};
-use rocket_sync_db_pools::{diesel as rkt_dsl, database};
-use diesel::prelude::*;
-use diesel::Queryable;
-use chrono::{DateTime, Utc};
+use rocket::{get, launch};
+use rocket_okapi::swagger_ui::{make_swagger_ui, SwaggerUIConfig};
+use rocket_okapi::{openapi, openapi_get_routes, JsonSchema};
+use rocket_sync_db_pools::{database, diesel as rkt_dsl};
 
 mod schema;
 
 #[database("my_db")]
 struct DbConn(rkt_dsl::PgConnection);
 
-#[derive(Debug, Queryable)]
+#[derive(Debug, Queryable, JsonSchema)]
 struct Greeting {
   id: i32,
   greeting: String,
-  created_at: DateTime<Utc>
+  created_at: DateTime<Utc>,
 }
 
+#[openapi]
 #[get("/")]
 async fn index(conn: DbConn) -> String {
   use self::schema::greetings::dsl::*;
-  conn.run(|c| {
-    let result = greetings.load::<Greeting>(c);
-    format!("{:?}", result)
-  }).await
+  conn
+    .run(|c| {
+      let result = greetings.load::<Greeting>(c);
+      format!("{:?}", result)
+    })
+    .await
 }
 
 #[launch]
 fn rocket() -> _ {
   // Build config map for db
-  let db : Map<_, Value> = map! {
+  let db: Map<_, Value> = map! {
     "url" => "postgres://postgres:postgres@localhost:5432/postgres".into()
   };
   // Add it to the config as "my_db"
   let figment = rocket::Config::figment().merge(("databases", map!["my_db" => db]));
   // Use custom config in favor of the regular `.build()`
-  rocket::custom(figment).mount("/", routes![index]).attach(DbConn::fairing())
+  rocket::custom(figment)
+    .mount("/", openapi_get_routes![index])
+    .mount("/swagger", make_swagger_ui(&SwaggerUIConfig {
+      url: "../openapi.json".to_string(),
+      ..Default::default()
+    }))
+    .attach(DbConn::fairing())
 }
